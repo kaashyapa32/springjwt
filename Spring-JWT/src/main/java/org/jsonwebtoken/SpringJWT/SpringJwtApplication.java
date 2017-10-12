c5df3a7fcb1ad47476b19e1157f5904240399b16
Pramod.Ramanth <Pramod.Ramanth@IN2228357W1.ey.net>
2017-10-12 12:21:22 +0530

    First Commit

---
+++ Spring-JWT/src/main/java/org/jsonwebtoken/SpringJWT/SpringJwtApplication.java
@@ -0,0 +1,12 @@
+package org.jsonwebtoken.SpringJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtApplication.class, args);
+	}
+}
---
+++ Spring-JWT/src/main/java/org/jsonwebtoken/SpringJWT/model/AuthorityName.java
@@ -0,0 +1,5 @@
+package org.jsonwebtoken.SpringJWT.model;
+
+public enum AuthorityName {
+    ROLE_USER, ROLE_ADMIN
+}
